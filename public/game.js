b9c4fd411da47f647aad16e0a2a3b3cd495e2a73
tatarski.dim <tatarski.dim@gmail.com>
2023-01-28 15:54:07 +0200

    Add comments, refactor code

--- public/game.js
+++ public/game.js
@@ -1,6 +1,14 @@
 let ground, gold_texture, monetki = [];
-let player, phi = 0, theta = 0;
+let player;
 let chad_texture;
+
+let t = 0;
+const isPressed = [];
+
+// Phi and Theta
+let horizontal_angle = 0,
+    vertical_angle = 0;
+
 function addMonetka(x,y,z) {
     let monetka_geometry = new THREE.CylinderGeometry(0.5, 0.5, 0.1, 7, 1);
     let monetka_material = new THREE.MeshPhongMaterial({color:"yellow", map:gold_texture});
@@ -18,6 +26,8 @@ function addIgrach(x, y, z) {
     player = mesh;
     return mesh;
 }
+
+
 function init() {
     // Iron wall texture
     gold_texture = new THREE.TextureLoader().load("./public/images/monetka.png");
@@ -52,12 +62,17 @@ function init() {
     iron_texture.wrapT = THREE.RepeatWrapping;
     iron_texture.repeat.set(1, 1);
 
+    // Grass material
     const grass_mat = new THREE.MeshPhongMaterial({ color: "white", map: grass_texture});
+
+    // Maze walls material
+    let maze_wall_material = new THREE.MeshPhongMaterial({color: "white", map: iron_texture});
 
     // Create ground
     ground = new THREE.Mesh(box_geom, grass_mat);
     ground.scale.set(10, 10, 1);
 
+    // Create maze
     let maze = [
         [1, 1, 1, 1, 0],
         [0, 0, 1, 1, 0],
@@ -66,7 +81,6 @@ function init() {
         [1, 1, 1, 1, 0]
     ]
     let maze_walls = [];
-    let maze_wall_material = new THREE.MeshPhongMaterial({color: "white", map: iron_texture});
     for(let i = 0; i < maze.length; i++) {
         maze_walls[i] = [];
         for(let j = 0; j < maze[i].length; j++) {
@@ -77,6 +91,7 @@ function init() {
             }
         }
     }
+
     // Point light
     const l1 = new THREE.PointLight("white", 0.4);
     l1.position.set(-1, -2, 4);
@@ -89,47 +104,76 @@ function init() {
     l3.position.set(-4, -4, 5);
     l3.lookAt(0, 0, 0);
 
-    // Add all objects to scene
-    scene.add(l1, l2, l3, ground);
 
+    // Add coins
     for(let i = 0; i < 10; i++) {
         monetki[i] = addMonetka(Math.random()*10 - 5, Math.random()*10 - 5, 2);
     }
+    
     addIgrach(0, 1, 1);
+
+    // Add all objects to scene
+    scene.add(l1, l2, l3, ground);
+
+    // Make all values of isPressed array equal to false
+    initIsPressed();
+
+    // Zelka
+    window.addEventListener("keydown", pressButton);
+    window.addEventListener("keyup", unpressButton)
 }
 
-let t = 0;
 function update() {
     t++;
+
+    // Rotate and wiggle coins
     for(let i in monetki) {
         monetki[i].rotation.z += 0.01;
         monetki[i].position.z = 2 + Math.sin(t/30)*0.5;
     }
-    // controls.update();
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
 
-    // if(zadurjanLiEW) { murdane}
-}
-function onKeyUp(e) {
-    if(e.keyCode == 49) {
-        camera.position.set(5,5,3);
-        camera.up.set(0, 0, 1);
-        camera.lookAt(0, 0, 0);
+    // Napravete tezi dva ifa v 1 red kod :) (ternary operator)
+    if(isPressed[65]) { // A
+        horizontal_angle += 0.05;
     }
-    if(e.keyCode == 50) {
-        camera.position.set(0,0,9);
-        camera.up.set(0, 0, 1);
-        camera.lookAt(0, 0, 0);
+    if(isPressed[68]) { // D
+        horizontal_angle -= 0.05;
     }
-    if(e.keyCode == 87) { // W
-        // zadurjanLiEW = false
-    }
-}
-window.addEventListener("keyup",
-    onKeyUp);
 
-function onKeyDown(e) {
+    // Napravete tezi dva ifa v 2 reda kod :) (ternary operator)
+    if(isPressed[87]) { // W
+        player.position.x += 0.1*Math.cos(horizontal_angle)
+        player.position.y += 0.1*Math.sin(horizontal_angle)
+    }
+    if(isPressed[83]) { // S
+        player.position.x -= 0.1*Math.cos(horizontal_angle)
+        player.position.y -= 0.1*Math.sin(horizontal_angle)
+    }
+    
+
+    // Move and update camera after moving player
+    camera.position.set(player.position.x, player.position.y, player.position.z);
+    camera.up.set(0, 0, 1);
+    camera.rotation.set(Math.PI/2, -Math.PI/2 + horizontal_angle, 0);
+
+}
+
+// Make all values of isPressed array equal to false
+function initIsPressed() {
+    for(let i = 0; i < 1000000000; i++) {
+        isPressed[i] = false;
+    }
+}
+
+function pressButton(e) {
+    isPressed[e.keyCode] = true;
+}
+
+function unpressButton(e) {
+    isPressed[e.keyCode] = false;
+}
+
+function movePlayer(e) {
     if(e.keyCode == 87) { // W
         // zadurjanLiEW = true
         player.position.x += 0.1*Math.cos(phi);
@@ -146,5 +190,3 @@ function onKeyDown(e) {
     camera.up.set(0, 0, 1);
     camera.lookAt(next_x, next_y, next_z);
 }
-window.addEventListener("keydown",
-    onKeyDown);
